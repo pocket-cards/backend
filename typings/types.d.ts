9888459197a1882a3f62814f435056ca79f5c66f
wwalpha <wwalpha@gmail.com>
2019-05-24 12:42:53 +0900

    update

--- typings/types.d.ts
+++ typings/types.d.ts
@@ -1,4 +1,14 @@
-declare module "*.json" {
+declare module '*.json' {
   const value: any;
   export default value;
+}
+
+export interface Words_Item {
+  word: string;
+}
+
+export interface Groups_Item {
+  id: string;
+  last_time: string;
+  word: string;
 }
