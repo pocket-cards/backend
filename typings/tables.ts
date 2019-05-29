19045de78591814359f2d52ec75d669917f0aeee
wwalpha <wwalpha@gmail.com>
2019-05-29 21:03:20 +0900

    add table typings

---
+++ typings/tables.ts
@@ -0,0 +1,18 @@
+export interface GroupsItem {
+  id: string;
+  nextTime: string;
+  word?: string;
+}
+
+export interface WordsItem {
+  // 単語
+  word: string;
+  // 発音記号
+  pronounce?: string;
+  // 語彙（中国語）
+  vocChn?: string;
+  // 語彙（日本語）
+  vocJpn?: string;
+  // 音声ファイル
+  mp3?: string;
+}
--- typings/types.d.ts
+++ typings/types.d.ts
@@ -2,13 +2,3 @@ declare module '*.json' {
   const value: any;
   export default value;
 }
-
-export interface Words_Item {
-  word: string;
-}
-
-export interface Groups_Item {
-  id: string;
-  last_time: string;
-  word: string;
-}
