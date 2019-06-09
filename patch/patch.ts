340fd21070b59793e93ad2071fab2f7b8ea27fec
wwalpha <wwalpha@gmail.com>
2019-06-09 19:15:52 +0900

    add patch file

---
+++ patch/patch.ts
@@ -0,0 +1,216 @@
+import { DynamoDB, Polly, S3, Translate } from 'aws-sdk';
+import { v1 } from 'uuid';
+import moment = require('moment');
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+let pollyClient: Polly;
+let s3Client: S3;
+let translateClient: Translate;
+
+const PATH_PATTERN = 'audio';
+const WORDS_TABLE = 'PocketCards_Words';
+const IPA_URL = 'https://m1rb1oo72l.execute-api.ap-northeast-1.amazonaws.com/v1';
+const IPA_API_KEY = 'FWbGwQrIIw3oqqIWy8O7W8lubvbAIpcG2Whor10b';
+const MP3_BUCKET = 'pocket-cards-mp3';
+
+export const queryItem_groups = (table: string, groupId: string) =>
+  ({
+    TableName: table,
+    ProjectionExpression: 'word',
+    KeyConditionExpression: '#id = :id',
+    ExpressionAttributeNames: {
+      '#id': 'id',
+    },
+    ExpressionAttributeValues: {
+      ':id': groupId,
+    },
+  } as DynamoDB.DocumentClient.QueryInput);
+
+export const scanItem_words = (table: string) =>
+  ({
+    TableName: table,
+    ProjectionExpression: 'word',
+  } as DynamoDB.DocumentClient.ScanInput);
+
+const patch = async () => {
+  const client = new DynamoDB.DocumentClient({
+    region: 'ap-northeast-1',
+  });
+
+  const queryResult = await client.query(queryItem_groups('PocketCards_Groups', 'x001')).promise();
+  const scanResult = await client.scan(scanItem_words(WORDS_TABLE)).promise();
+
+  const groupItems = queryResult.Items;
+  const wordItems = scanResult.Items;
+
+  if (!wordItems || !groupItems) {
+    return;
+  }
+
+  const results = groupItems.filter(item => wordItems.find(subItem => subItem.word === item.word) === undefined).map(item => item.word);
+
+  console.log(results.length);
+
+  while (results.length > 0) {
+    const targets = results.length > 50 ? results.slice(0, 50) : results;
+
+    const taskArray = targets.map(item => Promise.all([getPronounce(item), getMP3(item), getTranslate(item, 'zh'), getTranslate(item, 'ja')]));
+
+    const result = await Promise.all(taskArray);
+
+    console.log('単語情報を収集しました.');
+
+    // 単語辞書登録
+    const putTasks = result.map(item => {
+      const pronounce = item[0];
+      const mp3 = item[1];
+      const vocChn = item[2];
+      const vocJpn = item[3];
+
+      return client
+        .put(
+          putItem_words(WORDS_TABLE, {
+            word: pronounce['word'],
+            pronounce: pronounce['pronounce'],
+            mp3,
+            vocChn,
+            vocJpn,
+          })
+        )
+        .promise();
+    });
+
+    // 辞書登録処理
+    await Promise.all(putTasks);
+  }
+};
+
+export const putItem_words = (tableName: string, word: any) =>
+  ({
+    TableName: tableName,
+    Item: word,
+  } as DynamoDB.DocumentClient.PutItemInput);
+
+export const axiosGet = (url: string, config?: AxiosRequestConfig) =>
+  new Promise<AxiosResponse<any>>((resolve, reject) => {
+    axios
+      .get(url, config)
+      .then(value => resolve(value))
+      .catch(err => reject(err));
+  });
+
+export const axiosPost = (url: string, config?: AxiosRequestConfig) =>
+  new Promise<AxiosResponse<any>>((resolve, reject) => {
+    axios
+      .post(url, config)
+      .then(value => resolve(value))
+      .catch(err => reject(err));
+  });
+
+const getPronounce = async (word: string) => {
+  const res = await axiosGet(`${IPA_URL}?word=${word}`, {
+    headers: {
+      'x-api-key': IPA_API_KEY,
+    },
+  });
+
+  return res.data;
+};
+
+const getMP3 = async (word: string): Promise<string> => {
+  const client = polly(pollyClient);
+
+  /**  */
+  const request: Polly.SynthesizeSpeechInput = {
+    Text: word,
+    TextType: 'text',
+    VoiceId: 'Joanna',
+    OutputFormat: 'mp3',
+    LanguageCode: 'en-US',
+  };
+
+  const response = await client.synthesizeSpeech(request).promise();
+
+  // ファイル名
+  const filename: string = `${v1()}.mp3`;
+  const prefix: string = `${moment().format('YYYYMMDD')}`;
+  const key: string = `${PATH_PATTERN}/${prefix}/${filename}`;
+
+  const putRequest: S3.Types.PutObjectRequest = {
+    Bucket: MP3_BUCKET,
+    Key: key,
+    Body: response.AudioStream,
+  };
+
+  const sClient = s3(s3Client);
+  // S3に保存する
+  await sClient.putObject(putRequest).promise();
+
+  return key;
+};
+
+const getTranslate = async (word: string, targetLanguageCode: string): Promise<string> => {
+  const client = translate(translateClient);
+
+  const request: Translate.TranslateTextRequest = {
+    SourceLanguageCode: 'en',
+    TargetLanguageCode: targetLanguageCode,
+    Text: word,
+  };
+
+  const response = await client.translateText(request).promise();
+
+  return response.TranslatedText;
+};
+
+/** Dynamodb Client初期化 */
+export const dynamoDB = (client: DynamoDB.DocumentClient): DynamoDB.DocumentClient => {
+  if (client) return client;
+
+  return new DynamoDB.DocumentClient({
+    region: 'ap-northeast-1',
+  });
+};
+
+/** Polly Client初期化 */
+export const polly = (client: Polly, options?: Polly.ClientConfiguration): Polly => {
+  // 初期化済み
+  if (client) return client;
+
+  // 初期化設定あり
+  if (options) return new Polly(options);
+
+  return new Polly({
+    region: 'ap-northeast-1',
+  });
+};
+
+/** S3 Client初期化 */
+export const s3 = (client: S3, options?: S3.ClientConfiguration): S3 => {
+  // 初期化済み
+  if (client) return client;
+
+  // 初期化設定あり
+  if (options) return new S3(options);
+
+  // 初期化設定なし
+  return new S3({
+    region: 'ap-northeast-1',
+  });
+};
+
+/** S3 Client初期化 */
+export const translate = (client: Translate, options?: Translate.ClientConfiguration): Translate => {
+  // 初期化済み
+  if (client) return client;
+
+  // 初期化設定あり
+  if (options) return new Translate(options);
+
+  // 初期化設定なし
+  return new Translate({
+    region: 'ap-northeast-1',
+  });
+};
+
+patch();
---
+++ patch/resetTimes.ts
@@ -0,0 +1,53 @@
+import { DynamoDB } from 'aws-sdk';
+
+const start = async () => {
+  const client = new DynamoDB.DocumentClient({
+    region: 'ap-northeast-1',
+  });
+
+  const queryResult = await client.query(queryItem_groups('PocketCards_Groups', 'x001')).promise();
+
+  if (!queryResult.Items) return;
+
+  for (const idx in queryResult.Items) {
+    const item = queryResult.Items[idx];
+
+    await client.update(updateItem_groups('PocketCards_Groups', 'x001', item.word)).promise();
+
+    console.log(111);
+  }
+};
+
+export const queryItem_groups = (table: string, groupId: string) =>
+  ({
+    TableName: table,
+    ProjectionExpression: 'word',
+    KeyConditionExpression: '#id = :id',
+    FilterExpression: '#times <> :times',
+    ExpressionAttributeNames: {
+      '#id': 'id',
+      '#times': 'times',
+    },
+    ExpressionAttributeValues: {
+      ':id': groupId,
+      ':times': 0,
+    },
+  } as DynamoDB.DocumentClient.QueryInput);
+
+export const updateItem_groups = (table: string, id: string, word: string) =>
+  ({
+    TableName: table,
+    Key: {
+      id,
+      word,
+    },
+    UpdateExpression: 'set #times = :times',
+    ExpressionAttributeNames: {
+      '#times': 'times',
+    },
+    ExpressionAttributeValues: {
+      ':times': 0,
+    },
+  } as DynamoDB.DocumentClient.UpdateItemInput);
+
+start();
