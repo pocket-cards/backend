f28d2c6d071bfef2e726b1bae26963d8fab7f44f
wwalpha <wwalpha@gmail.com>
2019-06-17 20:55:54 +0900

    add xray typings

--- typings/types.d.ts
+++ typings/types.d.ts
@@ -2,3 +2,9 @@ declare module '*.json' {
   const value: any;
   export default value;
 }
+
+declare module 'aws-xray-sdk' {
+  export function captureAWSClient<T>(client: T): T;
+
+  export function captureAWS<T>(sdk: T): T;
+}
