30fc70355667737a187f3d81d460eb84fe87c92a
wwalpha <wwalpha@gmail.com>
2019-06-17 20:56:11 +0900

    remove functions

--- src/Z0/utils.ts
+++ src/Z0/utils.ts
@@ -1,21 +1,4 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import moment = require('moment');
-
-export const axiosGet = (url: string, config?: AxiosRequestConfig) =>
-  new Promise<AxiosResponse<any>>((resolve, reject) => {
-    axios
-      .get(url, config)
-      .then(value => resolve(value))
-      .catch(err => reject(err));
-  });
-
-export const axiosPost = (url: string, config?: AxiosRequestConfig) =>
-  new Promise<AxiosResponse<any>>((resolve, reject) => {
-    axios
-      .post(url, config)
-      .then(value => resolve(value))
-      .catch(err => reject(err));
-  });
 
 export const getNow = () => `${moment().format('YYYYMMDD')}`;
 
