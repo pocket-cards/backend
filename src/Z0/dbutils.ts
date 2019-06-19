e11d1afaa21423642f0af20581de70d304b04dff
wwalpha <wwalpha@gmail.com>
2019-06-19 23:22:36 +0900

    update utils

--- src/Z0/dbutils.ts
+++ src/Z0/dbutils.ts
@@ -23,7 +23,15 @@ export const put = (input: DynamoDB.DocumentClient.PutItemInput) => {
   return dynamoDB().put(input);
 };
 
-/** Put */
+export const putAsync = async (input: DynamoDB.DocumentClient.PutItemInput) => {
+  const result = put(input).promise();
+
+  console.log(result);
+
+  return result;
+};
+
+/** Query */
 export const query = (input: DynamoDB.DocumentClient.QueryInput) => {
   console.log(input);
 
@@ -34,7 +42,53 @@ export const queryAsync = async (input: DynamoDB.DocumentClient.QueryInput) => {
   // クエリ実行
   const result = await query(input).promise();
 
+  // 検索結果出力
   console.log(result);
+
+  if (result.LastEvaluatedKey) {
+    const lastResult = await queryAsync({ ...input, ExclusiveStartKey: result.LastEvaluatedKey });
+
+    if (result.Items && lastResult.Items) {
+      result.Items = result.Items.concat(lastResult.Items);
+    }
+    if (result.Count && lastResult.Count) {
+      result.Count = result.Count + lastResult.Count;
+    }
+    if (result.ScannedCount && lastResult.ScannedCount) {
+      result.ScannedCount = result.ScannedCount + lastResult.ScannedCount;
+    }
+  }
+
+  return result;
+};
+
+/** Scan */
+export const scan = (input: DynamoDB.DocumentClient.QueryInput) => {
+  console.log(input);
+
+  return dynamoDB().scan(input);
+};
+
+export const scanAsync = async (input: DynamoDB.DocumentClient.QueryInput) => {
+  // クエリ実行
+  const result = await scan(input).promise();
+
+  // 検索結果出力
+  console.log(result);
+
+  if (result.LastEvaluatedKey) {
+    const lastResult = await scanAsync({ ...input, ExclusiveStartKey: result.LastEvaluatedKey });
+
+    if (result.Items && lastResult.Items) {
+      result.Items = result.Items.concat(lastResult.Items);
+    }
+    if (result.Count && lastResult.Count) {
+      result.Count = result.Count + lastResult.Count;
+    }
+    if (result.ScannedCount && lastResult.ScannedCount) {
+      result.ScannedCount = result.ScannedCount + lastResult.ScannedCount;
+    }
+  }
 
   return result;
 };
