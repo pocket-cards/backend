9ff3010b553d957563c567f5f64c6e4f77cd2433
wwalpha <wwalpha@gmail.com>
2019-06-28 17:31:56 +0900

    bug fix

---
+++ src/D0/D002/app.ts
@@ -0,0 +1,124 @@
+import { Rekognition, S3 } from 'aws-sdk';
+import { APIGatewayEvent } from 'aws-lambda';
+import * as short from 'short-uuid';
+import { D001Response, D001Request } from '@typings/api';
+import { getNow } from '@utils/utils';
+
+// Rekognition
+let rekognitionClient: Rekognition;
+// S3
+let s3Client: S3;
+
+/** S3_BUCKET */
+const bucket = process.env.IMAGE_BUCKET as string;
+
+export const app = async (event: APIGatewayEvent): Promise<D001Response> => {
+  if (!event.body) {
+    return EmptyResponse();
+  }
+
+  const input = JSON.parse(event.body) as D001Request;
+
+  // save data in s3
+  // const params = {
+  //   Bucket: bucket,
+  //   Key: `${getNow()}/${short.generate()}`,
+  //   ContentType: input.type,
+  //   Body: new Buffer(input.image, 'base64'),
+  // };
+
+  // // S3 Client初期化
+  // if (!s3Client) {
+  //   s3Client = new S3({
+  //     region: process.env.AWS_REGION,
+  //   });
+  // }
+
+  // // S3に保存する
+  // await s3Client.putObject(params).promise();
+
+  const request: Rekognition.Types.DetectTextRequest = {
+    Image: {
+      S3Object: {
+        Bucket: bucket,
+        // Name: params.Key,
+        Name: '20190623/6NoHNRa3aXDzHmiCwGvD1g',
+      },
+    },
+  };
+  // S3 Client初期化
+  if (!rekognitionClient) {
+    rekognitionClient = new Rekognition({
+      region: process.env.AWS_REGION,
+    });
+  }
+
+  console.log(request);
+
+  const result = await rekognitionClient.detectText(request).promise();
+
+  // console.log(result);
+  // 認識結果なし
+  if (!result.TextDetections) {
+    return EmptyResponse();
+  }
+
+  const results: string[] = [];
+
+  result.TextDetections.forEach(item => {
+    // 文字列検索できない
+    if (!item.DetectedText) return;
+    // 対象外を除く
+    if (!filter(item)) return;
+
+    results.push(item.DetectedText);
+  });
+
+  // 重複結果を削除してから返却する
+  const words = Array.from(new Set(results));
+
+  return {
+    count: words.length,
+    words,
+  };
+};
+
+// 単語以外のデータを除く
+const filter = (item: Rekognition.TextDetection): boolean => {
+  // 確信度９０以下を除く
+  if (item.Confidence && item.Confidence < 90) {
+    return false;
+  }
+  // 必須チェック
+  if (!item.DetectedText) return false;
+
+  const text = item.DetectedText;
+
+  // // 行の場合、対象外単語を含めた場合
+  if (item.Type === 'LINE') {
+    console.log(item.DetectedText, item.Confidence);
+    //   // if (excludeWord.find(word => text.includes(word))) {
+    //   //   if (item.Id !== undefined) {
+    //   //     excludeId.push(item.Id);
+    //   //   }
+    //   //   return false;
+    //   // }
+  }
+
+  // // 対象外のLineの子供も対象外
+  // if (item.ParentId !== undefined && excludeId.includes(item.ParentId)) {
+  //   return false;
+  // }
+
+  // 2桁以下の対象外
+  if (text.length <= 2) return false;
+
+  const regex = new RegExp(/^[a-zA-Z]+$/);
+
+  return regex.test(text);
+};
+
+const EmptyResponse = (): D001Response => ({
+  count: 0,
+  words: [],
+});
