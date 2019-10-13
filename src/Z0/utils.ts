0a5bb5d427e6507c588107f68dcba804411eb460
wwalpha <wwalpha@gmail.com>
2019-10-13 22:18:55 +0900

    add utils

--- src/Z0/utils.ts
+++ src/Z0/utils.ts
@@ -1,4 +1,5 @@
 import moment = require('moment');
+import { APIGatewayEvent } from 'aws-lambda';
 
 export const getNow = () => `${moment().format('YYYYMMDD')}`;
 
@@ -19,3 +20,39 @@ export const getNextTime = (times: number) => {
 
 // Sleep
 export const sleep = (timeout: number) => new Promise(resolve => setTimeout(() => resolve(), timeout));
+
+/**
+ * Header情報からUserIdを取得する(Cognito Authorization IdToken)
+ *
+ * @param event APIGateway EVENT
+ * @param authKey Header Key
+ */
+export const getUserId = (event: APIGatewayEvent, authKey: string = 'Authorization') => {
+  const value = event.headers[authKey];
+
+  // データが存在しない場合、エラーとする
+  if (!value) {
+    throw new Error('Can not found User Id.');
+  }
+
+  try {
+    // {
+    //   sub: 'cbff371c-ef97-4afc-b6a3-166c2163307e',
+    //   aud: 'relm874c6hkjouqalvvq0utth',
+    //   email_verified: true,
+    //   token_use: 'id',
+    //   auth_time: 1570971688,
+    //   iss: 'https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_Pn4pwBiY0',
+    //   'cognito:username': 'wwalpha',
+    //   exp: 1570975288,
+    //   iat: 1570971688,
+    //   email: 'wwalpha@gmail.com'
+    // }
+    const userInfo = JSON.parse(Buffer.from(value, 'base64').toString());
+
+    return userInfo['cognito:username'];
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
