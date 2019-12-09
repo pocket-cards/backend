ea2995e38c0d3ebd375203a36bd441a29b82e43c
wwalpha <wwalpha@gmail.com>
2019-12-09 18:37:19 +0900

    remove function B001

--- infra/lambda_B0.tf
+++ infra/lambda_B0.tf
@@ -1,29 +1,29 @@
 # -----------------------------------------------
 # グループ登録: /groups
 # -----------------------------------------------
-module "b001" {
-  source = "github.com/wwalpha/terraform-module-lambda"
+# module "b001" {
+#   source = "github.com/wwalpha/terraform-module-lambda"
 
-  function_name         = local.lambda.b001.function_name
-  alias_name            = local.lambda_alias_name
-  handler               = local.lambda_handler
-  runtime               = local.lambda_runtime
-  memory_size           = 512
-  role_name             = local.lambda.b001.role_name
-  role_policy_json      = [file("iam/lambda_policy_dynamodb.json")]
-  log_retention_in_days = var.lambda_log_retention_in_days
-  source_dir            = local.lambda.b001.source_dir
-  source_output_path    = local.lambda.b001.source_output_path
+#   function_name         = local.lambda.b001.function_name
+#   alias_name            = local.lambda_alias_name
+#   handler               = local.lambda_handler
+#   runtime               = local.lambda_runtime
+#   memory_size           = 512
+#   role_name             = local.lambda.b001.role_name
+#   role_policy_json      = [file("iam/lambda_policy_dynamodb.json")]
+#   log_retention_in_days = var.lambda_log_retention_in_days
+#   source_dir            = local.lambda.b001.source_dir
+#   source_output_path    = local.lambda.b001.source_output_path
 
-  variables = {
-    TABLE_HISTORY     = local.dynamodb_name_history
-    TABLE_USER_GROUPS = local.dynamodb_name_user_groups
-    TABLE_GROUP_WORDS = local.dynamodb_name_group_words
-    TZ                = local.timezone
-  }
+#   variables = {
+#     TABLE_HISTORY     = local.dynamodb_name_history
+#     TABLE_USER_GROUPS = local.dynamodb_name_user_groups
+#     TABLE_GROUP_WORDS = local.dynamodb_name_group_words
+#     TZ                = local.timezone
+#   }
 
-  layers = [local.xray, local.moment, local.lodash]
-}
+#   layers = [local.xray, local.moment, local.lodash]
+# }
 
 # -----------------------------------------------
 # グループ情報取得: /groups/{groupId}
