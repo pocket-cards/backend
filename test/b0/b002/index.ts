2d3897acbb2492c83be2b6485301dd562787bedf
EC2 Default User <ec2-user@ip-172-31-40-154.ap-northeast-1.compute.internal>
2020-07-16 04:07:55 +0000

    PCA-8

--- test/b0/b002/index.ts
+++ test/b0/b002/index.ts
@@ -1,24 +1,61 @@
 import chai from 'chai';
+import chaiExclude from 'chai-exclude';
 import chaiHttp from 'chai-http';
 import server from '@app';
+import AWS from 'aws-sdk';
+import AWSMock from 'aws-sdk-mock';
+import { HEADER_AUTH } from '../../Commons';
 
 chai.use(chaiHttp);
+chai.use(chaiExclude);
 chai.should();
 
 describe('B002', () => {
   const URL = '/groups';
 
-  it('Case001', async () => {
-    const res = await chai.request(server).get(URL).set('authorization', 'B002').send();
+  it('Case001: Get List Success', async () => {
+    // mock prepare
+    AWSMock.mock(
+      'DynamoDB.DocumentClient',
+      'query',
+      (params: AWS.DynamoDB.DocumentClient.QueryInput, callback: any) => {
+        const output: AWS.DynamoDB.DocumentClient.QueryOutput = {
+          Count: 0,
+          Items: require('./datas/db001.json'),
+        };
+
+        callback(null, output);
+      }
+    );
+
+    const res = await chai.request(server).get(URL).set('authorization', HEADER_AUTH).send();
 
     // found 2 records
     chai.expect(res.body).to.be.deep.eq(require('./datas/res001.json'));
   });
 
-  it('Case002', async () => {
-    const res = await chai.request(server).get(URL).set('authorization', 'B999').send();
+  it('Case002: Empty List', async () => {
+    // mock prepare
+    AWSMock.mock(
+      'DynamoDB.DocumentClient',
+      'query',
+      (params: AWS.DynamoDB.DocumentClient.QueryInput, callback: any) => {
+        const output: AWS.DynamoDB.DocumentClient.QueryOutput = {
+          Count: 0,
+          Items: [],
+        };
+
+        callback(null, output);
+      }
+    );
+    const res = await chai.request(server).get(URL).set('authorization', HEADER_AUTH).send();
 
     // not found
-    chai.expect(res.body).is.empty;
+    chai.expect(res.body).to.be.deep.eq(require('./datas/res002.json'));
   });
+});
+
+afterEach(() => {
+  // mock clear
+  AWSMock.restore('DynamoDB.DocumentClient');
 });
