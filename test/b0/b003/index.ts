07811ae64293a3f5d071f44482075558c663fd9c
EC2 Default User <ec2-user@ip-172-31-40-154.ap-northeast-1.compute.internal>
2020-07-16 04:25:57 +0000

    PCA-57

--- test/b0/b003/index.ts
+++ test/b0/b003/index.ts
@@ -1,15 +1,38 @@
 import chai from 'chai';
+import chaiExclude from 'chai-exclude';
 import chaiHttp from 'chai-http';
 import server from '@app';
+import AWS from 'aws-sdk';
+import AWSMock from 'aws-sdk-mock';
+import { HEADER_AUTH } from '../../Commons';
 
 chai.use(chaiHttp);
+chai.use(chaiExclude);
 chai.should();
 
 describe('B003', () => {
   it('Case001', async () => {
+    // mock prepare
+    AWSMock.mock(
+      'DynamoDB.DocumentClient',
+      'get',
+      (params: AWS.DynamoDB.DocumentClient.GetItemInput, callback: any) => {
+        const output: AWS.DynamoDB.DocumentClient.GetItemOutput = {
+          Item: require('./datas/db001.json'),
+        };
+
+        callback(null, output);
+      }
+    );
+
     const URL = '/groups/B003';
     const res = await chai.request(server).get(URL).set('authorization', 'B003').send();
 
     chai.expect(res.body).to.be.deep.eq(require('./datas/res001.json'));
   });
+});
+
+afterEach(() => {
+  // mock clear
+  AWSMock.restore('DynamoDB.DocumentClient');
 });
