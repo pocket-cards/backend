4903a1192be445c3cf05dcc21554825315209f9e
EC2 Default User <ec2-user@ip-172-31-40-154.ap-northeast-1.compute.internal>
2020-08-13 02:35:45 +0000

    update test case

---
+++ test/server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import BodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(BodyParser.json({ limit: '50mb' }));
+app.use(BodyParser.urlencoded({ extended: true }));
+
+app.post('/image2words', express.json(), (req, res) => {
+  res.send(['First', 'Things', 'Lesson', 'Excuse', 'your', 'possessive', 'adjective', 'handbag', 'very', 'much', 'this', 'book', 'watch', 'dress', 'shirt']);
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.info(`${req.method} ${req.originalUrl}`);
+
+  next();
+});
+
+export default app;
