080ae9819eda7bb36f7ebfff0197513cc30b2d88
EC2 Default User <ec2-user@ip-172-31-40-154.ap-northeast-1.compute.internal>
2020-08-16 03:20:53 +0000

    PCA-79

--- src/ecs/c0/c005/index.ts
+++ src/ecs/c0/c005/index.ts
@@ -3,10 +3,8 @@ import { C005Response, C005Params } from 'typings/api';
 import { DBHelper } from '@utils';
 import { Words } from '@queries';
 
-export default async (req: Request): Promise<C005Response> => {
-  const params = (req.params as unknown) as C005Params;
+export default async (req: Request<C005Params, any, any, any>): Promise<C005Response> => {
+  const params = req.params;
 
   await DBHelper().delete(Words.del({ id: params.word, groupId: params.groupId }));
-
-  return;
 };
---
+++ test/c0/c005/index.ts
@@ -0,0 +1,26 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import AWS from 'aws-sdk';
+import AWSMock from 'aws-sdk-mock';
+import server from '@app';
+import { HEADER_AUTH } from '@test/Commons';
+
+chai.use(chaiHttp);
+chai.should();
+
+AWSMock.setSDKInstance(AWS);
+
+describe('C005', () => {
+  it('Case001', async () => {
+    AWSMock.mock('DynamoDB.DocumentClient', 'delete', (params: any, callback: any) => {
+      chai.expect(require('./datas/input001.json')).to.be.deep.eq(params);
+
+      callback(null, 'success');
+    });
+
+    const URL = '/groups/C005/words/C005-1';
+    const res = await chai.request(server).delete(URL).set('authorization', HEADER_AUTH).send();
+
+    chai.expect(200).to.be.eq(res.status);
+  });
+});
